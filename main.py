7392761a59900e8e78edb0d2fb557b4353f13bb9
zqleow13 <hayleyleow13@gmail.com>
2023-11-02 16:19:14 +0800

    Imported async tele bot instead
    Wanted to convert my telebot to async so that it can run other operations while waiting

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import os #os gets environment variables
 from dotenv import load_dotenv #dotenv also gets environment variables
-import telebot
+from telebot.async_telebot import AsyncTeleBot
 import fastf1
 
 # Get token from env file
 load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
-bot = telebot.TeleBot(BOT_TOKEN)
+bot = AsyncTeleBot(BOT_TOKEN)
 
 # Message handlers = handles commands and then sends a msg
 # They define filters which a message must pass. If the message passes the filter, the function is called and the message is passed as an arg
@@ -20,7 +20,6 @@ def start_command(message):
         "To get the latest race results, press /raceresults. \n" +
         "To get help, press /help.")
     
-
 # fetch race results
 # show race results in a message
 
