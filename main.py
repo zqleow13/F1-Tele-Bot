7d0d01a333659982a85db41af01924114d057b4f
zqleow13 <hayleyleow13@gmail.com>
2023-11-20 16:06:27 +0800

    Debugged and improved code
    - Improved code logic to fetch race info as previously, the info could not be fetched.
    - Instead of pressing /raceresults, users can obtain results once they press /start

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ bot = AsyncTeleBot(BOT_TOKEN)
 user_chat_ids = {}
 
 # Function to fetch race information from the Ergast API
-def fetch_race_info(endpoint):
-    url = f'http://ergast.com/api/f1/{endpoint}.json'
+def fetch_race_info():
+    url = 'http://ergast.com/api/f1/current/last/results.json'
     try:
         response = requests.get(url)
         if response.status_code == 200:
@@ -29,37 +29,52 @@ def fetch_race_info(endpoint):
         return None
 
 # Function to send race information as a notification
-async def send_race_info_notification(chat_id, race_info, race_type):
+async def send_race_info_notification(chat_id, message_text):
     try:
-        message_text = f"Formula 1 {race_type} Race Information:\n"
+        results = fetch_race_info()
+        latest_race = results['MRData']['RaceTable']['Races'][0]['raceName']
+        message_text = f"Formula 1 {latest_race} Race Information:\n"
         
         # Add logic to format and extract relevant information from race_info
+        for result in results['MRData']['RaceTable']['Races'][0]['Results']:
+            position = result['position']
+            driver = result['Driver']['code']
+            outcome = result['positionText']
+            
+            result_str = f'{position}. {driver} ({outcome})\n'
+            
+            message_text += result_str
 
         await bot.send_message(chat_id, message_text)
     except Exception as e:
         print(f"An error occurred while sending race information notification: {str(e)}")
 
-# Function to periodically check for upcoming and past races
+# Function to periodically check for latest race
 async def check_race_info():
     while True:
-        # Fetch information about the next race
-        next_race_info = fetch_race_info('current/next')
-
         # Fetch information about the last race
-        last_race_info = fetch_race_info('current/last/results')
+        last_race_info = fetch_race_info()
+        
+        # Flag to track whether notifications have been sent
+        notifications_sent = False
 
         # Loop through stored user chat IDs and send notifications
         for user_id, chat_id in user_chat_ids.items():
-            # Send notification for the upcoming race
-            if next_race_info and 'RaceTable' in next_race_info['MRData']:
-                await send_race_info_notification(chat_id, next_race_info, 'Upcoming')
 
             # Send notification for the last race results
             if last_race_info and 'RaceTable' in last_race_info['MRData'] and 'Results' in last_race_info['MRData']['RaceTable']['Races'][0]:
-                await send_race_info_notification(chat_id, last_race_info, 'Latest Results')
+                await send_race_info_notification(chat_id, last_race_info)
+                notifications_sent = True # Set the flag to True after sending one notification
+        
+        # Check if notifications have been sent to all users
+        if notifications_sent == True:
+            break
 
         # Sleep for a period of time before checking again (adjust as needed)
-        await asyncio.sleep(3600)  # Sleep for 1 hour
+        await asyncio.sleep(10)  # Sleep for 30 seconds
+        
+    
+        
 
 # Handler for the /start command
 @bot.message_handler(commands=['start'])
@@ -69,7 +84,9 @@ async def handle_start(message):
     user_chat_ids[user_id] = message.chat.id
 
     # Send a welcome message
-    await bot.send_message(message.chat.id, "Welcome to the F1 Updates Bot!")
+    await bot.send_message(message.chat.id,
+                           "Welcome to the F1 Updates Bot!\n"
+                           "Here are the latest F1 race results!")
 
 # Start the event loop to run the asynchronous functions
 async def main():
